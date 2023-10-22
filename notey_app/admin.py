1014860ef351055587f2d450ea154c64d3b26af9
Anna Lorencová <a.lorencov@seznam.cz>
2023-10-22 22:04:00 +0200

    models created

--- notey_app/admin.py
+++ notey_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from notey_app.models import Mood, Note, Advice
 # Register your models here.
+
+admin.site.register(Mood)
+admin.site.register(Note)
+admin.site.register(Advice)
--- notey_app/models.py
+++ notey_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
@@ -9,9 +10,40 @@ from ckeditor.fields import RichTextField
 class Note(models.Model):
     create_date = models.DateTimeField(default=timezone.now)
     # By default, it takes timezone from settings.py
+    category = models.TextField
     title = models.CharField(max_length=225)
     text = RichTextField(blank=True, null=True)
 
     def publish(self):
         self.create_date = timezone.now()
         self.save()
+
+    def get_absolute_url(self):
+        return reverse('daily_advice')
+    # redirects to daily advice based on mood
+
+
+class Mood(models.Model):
+    mood_date = models.DateTimeField(default=timezone.now)
+    MOOD_CHOICES = (
+        ('angry', 'angry'),
+        ('annoyed', 'annoyed'),
+        ('sad', 'sad'),
+        ('very sad', 'very sad'),
+        ('fine', 'fine'),
+        ('mischievous', 'mischievous'),
+        ('excited', 'excited'),
+        ('playful', 'playful'),
+        ('boder', 'boder'),
+        ('sick', 'sick'),
+        ('sleepy', 'sleepy'),
+        ('surprised', 'surprised'),
+        ('unhappy', 'unhappy'),
+        ('grumpy', 'grumpy')
+    )
+    my_mood = models.CharField(max_length=25, choices=MOOD_CHOICES)
+
+
+class Advice(models.Model):
+    my_mood = models.ForeignKey('Mood', on_delete=models.CASCADE)
+    text_advice = models.TextField(max_length=600)
